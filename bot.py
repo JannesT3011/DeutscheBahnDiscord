03a08b3fed20dea51b6d413eec34689e155a842a
Jannes <36892200+JannesT3011@users.noreply.github.com>
2023-02-22 20:56:04 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands, tasks
+from config import *
+from datetime import datetime, timedelta
+
+
+class Bot(commands.AutoShardedBot):
+    def __init__(self, **kwargs):
+        intents = discord.Intents.all()
+        intents.message_content = True
+        super(Bot, self).__init__(
+            command_prefix="db.",
+            description=DESCRIPTION,
+            intents=intents,
+            activity=discord.Game(name=ACTIVITY)
+        )
+    
+    # STARTUP FUNCTIONS
+    async def startup(self) -> None:
+        await self.wait_until_ready()
+        await self.tree.sync()
+        self.ban_user_task.start()
+        self.leaderboard_task.start()
+       
+    async def load_cogs(self) -> None:
+        for ext in COGS:
+            try:
+                await self.load_extension(ext)
+                logger.info(f"{ext} loaded!")
+            except Exception as e:
+                print(f"Cant load {ext}")
+                raise e
+        
+    async def setup_hook(self) -> None:
+        await self.load_cogs()
+        # self.tree.on_error = self.on_app_command_error
+        self.loop.create_task(self.startup())
+
+    
+    # EVENTS:
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+
+
+bot = Bot()
+
+bot.run(TOKEN, reconnect=True)
