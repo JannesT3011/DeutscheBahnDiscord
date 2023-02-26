8db6738722201d4a25c8532cd94ec1e9f20b3ff5
jannes.t13@gmail.com <jannes.t13@gmail.com>
2023-02-26 15:55:25 +0100

    Add config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = ""
+COGS = [
+    "cogs.route",
+    "cogs.traininfo",
+    "cogs.departures",
+    "cogs.help"
+]
+
+# Bot misc
+ACTIVITY = "Deutsche Bahn Bot"
+DESCRIPTION= "DeutscheBahn Bot by Bambus#8446"
+EMBED_COLOR = "#a21917"
