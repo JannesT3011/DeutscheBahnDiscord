3f64d45a16f381d74c936f09c2bee1f51436ed81
JannesT3011 <jannes.t13gmail.com>
2023-02-26 20:24:48 +0000

    Currently disable logger

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from config import *
 from discord import Color
 import logging
 
-logging.basicConfig(filename="logging.log", encoding='utf-8', level=logging.DEBUG)
+#logging.basicConfig(filename="logging.log", encoding='utf-8', level=logging.DEBUG)
 
 class Bot(commands.AutoShardedBot):
     def __init__(self, **kwargs):
@@ -17,27 +17,27 @@ class Bot(commands.AutoShardedBot):
             activity=discord.Game(name=ACTIVITY)
         )
         self.embed_color = Color.from_str(EMBED_COLOR)
-    
+
     # STARTUP FUNCTIONS
     async def startup(self) -> None:
         await self.wait_until_ready()
         await self.tree.sync()
-       
+
     async def load_cogs(self) -> None:
         for ext in COGS:
             try:
                 await self.load_extension(ext)
                 print(f"{ext} loaded!")
-                logging.info(f"{ext} loaded!")
+                #logging.info(f"{ext} loaded!")
             except Exception as e:
-                logging.error(f"Cant load {ext}")
+                #logging.error(f"Cant load {ext}")
                 raise e
-        
+
     async def setup_hook(self) -> None:
         await self.load_cogs()
         self.tree.on_error = self.on_app_command_error
         self.loop.create_task(self.startup())
-    
+
     # ERROR HANDLER:
     async def on_app_command_error(self, interaction: discord.Interaction, error):
         if isinstance(error, discord.app_commands.errors.CommandInvokeError):
