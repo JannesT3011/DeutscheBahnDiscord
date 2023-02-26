9cddc7b8f8b0f96a6c7f82f9a3ed9b2e323a59fa
jannes.t13@gmail.com <jannes.t13@gmail.com>
2023-02-26 15:54:38 +0100

    Add /help

---
+++ cogs/help.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name='help')
+    async def help_command(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            title="Deutsche Bahn Discord Bot - Help",
+            color=self.bot.embed_color,
+            description=f"**/route**\n**/traininfo**\n**/departures**"
+        )
+
+        return await interaction.response.send_message(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Help(bot))
