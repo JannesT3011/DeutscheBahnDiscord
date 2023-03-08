a9c3c79adf26169b030e166f7eccdcc71b7663b9
jannes.t13@gmail.com <jannes.t13@gmail.com>
2023-03-08 19:19:12 +0100

    Add str_to_time converter

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,13 +2,13 @@ from .get_route_data import get_station_info, get_journey_info
 from .get_trips_data import get_trip_id, get_trip_info
 from .get_stops_data import get_departure_data
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil import parser
 
-def format_dt(time):
+def format_dt(time) -> str:
     """
     FORMAT API TIME (e.g: 2023-02-25T16:48:00+01:00)
-    TO: "%d.%m.%y %H:%M" format
+    TO: "%d.%m.%y %H:%M" format (str)
     """
     dt_parser = parser.isoparse(time)
 
@@ -23,3 +23,10 @@ def calc_delay(planned: str, actual: str) -> int:
     actual_dt = datetime.strptime(actual, "%d.%m.%y %H:%M")
 
     return int((actual_dt - planned_dt).seconds / 60)
+
+def str_to_time(str_time) -> datetime: # Input: DD.MM.YY hh:mm
+    """
+    FORMAT API TIME TO DATETIME
+    TO: "%d.%m.%y %H:%M" format
+    """
+    return datetime.strptime(str_time, "%d.%m.%y %H:%M")
