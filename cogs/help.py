ed2e334eb46ff594be9713c49365cd55dfa31025
jannes.t13@gmail.com <jannes.t13@gmail.com>
2023-03-22 19:18:20 +0100

    Update and style help command

--- cogs/help.py
+++ cogs/help.py
@@ -1,20 +1,35 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+  
+
 
 class Help(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: commands.AutoShardedBot = bot
 
     @app_commands.command(name='help', description="View all commands")
     async def help_command(self, interaction: discord.Interaction):
         embed = discord.Embed(
-            title="Deutsche Bahn Discord Bot - Help",
+            title="ℹ️ DBot - Help ℹ️",
+            description="""
+            </help:1081581674724335709>
+            ↪️ View all commands
+            </route:1081581674724335706>
+            ↪️ Plan your DB route ||(e.g Hamburg to München)||
+            </departures:1081581674724335708>
+            ↪️ View the departure of given Station ||(e.g Hamburg Hbf)||
+            </traininfo:1081581674724335707>
+            ↪️ See infos about a given train ||(e.g ICE72)||
+            """,
             color=self.bot.embed_color,
-            description=f"**/route**\n**/traininfo**\n**/departures**"
+            url="https://github.com/JannesT3011/DeutscheBahnDiscord"
         )
+        embed.set_footer(text="DBot", icon_url=self.bot.user.display_avatar.url)
 
-        return await interaction.response.send_message(embed=embed)
+        
+        return await interaction.response.send_message(embed=embed, ephemeral=True)
+    
 
 async def setup(bot):
     await bot.add_cog(Help(bot))
